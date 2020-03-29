4da7c86fc68f4deec2a38ba459c0b9e5f5c846f8
gtcha2 <aron7628@gmail.com>
2020-03-28 19:03:24 -0500

    Subject: front end flask container file updates-- main.py and accessory functions
    main.py now imports accessory functions and calls a function to send data to an endpoint.
    started creating a cache object for this server to handle all the requests and ignore data.
    If person decides to look at a another query  in rapid fashion should be able to handle the overload of data.
    
    TODO: find a way to make python asynchronous with response calls
    Finish the cacheObject
    Start making a logging decorator for our python functions.

---
+++ front-end/accessoryFunctions.py
@@ -0,0 +1,25 @@
+import requests
+#import loggingFile
+# TODO: add log Decorators use functools etc
+# TODO: find the end points
+# TODO: init a dataCache instance. Cache data, 
+def postData2Svr(data, URLEndPoint):
+    ## need a wrapper to handle this
+    ## without a wrapper cannot run async
+    response = requests.post(url=URLEndPoint, data=data)
+    return response
+
+class dataCache:
+    """"supposedly hold the data, 
+    need this class to empty response data if client switches screens or searches
+    """
+
+    #will probably have json data
+    cache=[]
+    def __init__(self,):
+        ## set up class
+        return
+    def deleteCache(self):
+        self.cache=[]
+        return 
+    
--- front-end/main.py
+++ front-end/main.py
@@ -7,6 +7,8 @@
 from flask import Flask, render_template, request, jsonify
 #  import os
 
+#import threading
+import accessoryFunctions
 
 app = Flask(__name__)
 
@@ -14,15 +16,28 @@ app = Flask(__name__)
 def splashpage():
     return render_template("splash.html")
 
+
+
+urlTest="""http://ptsv2.com/t/9xfj1-1585431634/post"""
+
+
 @app.route('/results', methods=["POST"])
 def searchresults():
     search_string = request.form["searchbar"]
     print(search_string)
+    #postData2Svr(search_string,urlTest)
     #  TODO: second RESTful call to backend server
     #  TODO: send some placeholder blank images like all cool websites do nowadays
     #  and replace the blank images with javascript once the results/data from the call come back
     #  TODO: replace placeholder results page
     return render_template("searchresults.html", search_query=search_string)
+
+@app.route('/about', methods=["GET"])
+def about():
+    return render_template("about.html")
+
+
+
 
 
 if __name__ == "__main__":
